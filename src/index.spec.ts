f34c644cad57d3c2d8677c7ebd2274142f375205
Philip Niedertscheider <me@philprime.dev>
2023-02-22 18:40:39 +0100

    tests: added missing environment variables for smoke test (#18)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import * as cp from 'child_process';
 import * as path from 'path';
 import * as process from 'process';
@@ -5,6 +7,7 @@ import * as process from 'process';
 test('test runs', () => {
   process.env['INPUT_REPOSITORY'] = 'kula-app/remove-stale-workflow-runs';
   process.env['INPUT_MAX_RUNS_LIMIT'] = '5000';
+  process.env['INPUT_DRY_RUN'] = 'false';
   const ip = path.join(__dirname, '../', 'dist', 'index.js');
   const cmd = `node ${ip}`;
   try {
@@ -12,6 +15,8 @@ test('test runs', () => {
     console.log(result);
   } catch (error) {
     console.error(error);
-    fail(error);
+    console.error(error.stdout?.toString());
+    console.error(error.stderr?.toString());
+    throw error;
   }
 });
